eef3badb26873120d87adf105fd339aeebd029cf
marina <marinasolovieva189@gmail.com>
2024-05-26 12:46:39 +0300

    add example with green button

---
+++ 05-react-jsx-green-button/script.js
@@ -0,0 +1,19 @@
+// функциональный компонент, который возвращает JSX код
+const App = () => {
+  console.log('Called');
+  let buttonText = 'Click me please';
+  const onButtonClick = () => {
+    buttonText = 'Hello from React';
+    console.log(buttonText);
+  };
+
+  return (
+    <div className="app">
+      <button onClick={onButtonClick}>{buttonText}</button>
+    </div>
+  );
+};
+
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+root.render(<App />); // все пользовательские компоненты должны начинаться с большой буквы
